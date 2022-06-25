2e02a229b0f79618de5722f07a43e704845d92e4
Legend108 <80154701+Legend108@users.noreply.github.com>
2022-06-25 22:25:09 +0530

    Create message.ts

---
+++ events/message.ts
@@ -0,0 +1,28 @@
+import { Client, Message } from "revolt.js";
+// import client from "../index";
+
+export default {
+  name: "message",
+  run: async (client: Client, message: Message) => {
+    console.log("H");
+    const prefix = process.env.PREFIX! || "!";
+
+    if (message.content?.startsWith(prefix)) {
+      const commandName = message.content?.split(prefix)[1];
+
+      // @ts-ignore
+      const cmd = client.commands.get(commandName);
+
+      if (cmd) {
+        try {
+          cmd.run(client, message);
+        } catch (err) {
+          console.error(err);
+          await message.reply("Oops, an error occured");
+        }
+      } else {
+        return;
+      }
+    }
+  },
+};
