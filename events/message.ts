640407c8cda2b71df05ffbe2a35bc290fca83b17
Legend108 <80154701+Legend108@users.noreply.github.com>
2022-06-26 21:03:37 +0530

    Ft: Args support

--- events/message.ts
+++ events/message.ts
@@ -1,20 +1,42 @@
 import { Client, Message } from "revolt.js";
-// import client from "../index";
+import data from "../index";
 
 export default {
   name: "message",
   run: async (client: Client, message: Message) => {
-    const prefix = process.env.PREFIX! || "!";
+    let prefix: String;
 
+    const guildID = message.channel?.server_id;
+
+    const checkPrefix: Boolean = await data.db.has("prefix_" + guildID);
+
+    // @ts-ignore
+    prefix = checkPrefix
+      ? await data.db.get("prefix_" + guildID)
+      : process.env.PREFIX!;
+
+    // @ts-ignore
     if (message.content?.startsWith(prefix)) {
-      const commandName = message.content?.split(prefix)[1];
+      // @ts-ignore
+      const commandName: String = message.content?.replace(prefix, "");
+
+      const cmdName = commandName.split(" ")[0];
 
       // @ts-ignore
-      const cmd = client.commands.get(commandName);
+      const cmd = client.commands.get(cmdName);
 
       if (cmd) {
+        const args: Array<String> = [];
+        const argsF: Array<string> = commandName.split(" ");
+
+        argsF.filter((val: String, index: Number) => {
+          if (!(val == cmdName)) {
+            args.push(val);
+          }
+        });
+
         try {
-          cmd.run(client, message);
+          cmd.run(client, message, args, data.db);
         } catch (err) {
           console.error(err);
           await message.reply("Oops, an error occured");
